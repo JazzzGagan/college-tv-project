e38d21aafa9ee12591f9e88d48231bb46b365356
Gagan Sunar <gagaan.sunaar@gmail.com>
2025-11-21 15:30:24 +0545

    feat:home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,27 +3,32 @@ import { Routes, Route } from "react-router-dom";
 import AdminPanel from "./pages/AdminPanel";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./pages/Login";
-
-const queryClient = new QueryClient();
+import { ToastContainer } from "react-toastify";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route
-              path="/admin"
-              element={
-                <ProtectedRoute>
-                  <AdminPanel />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </BrowserRouter>
-      </QueryClientProvider>
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+      />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <AdminPanel />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </>
   );
 }
