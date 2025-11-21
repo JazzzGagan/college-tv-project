4651bffbe672c0c095c60ec66b7840918011130e
Gagan Sunar <gagaan.sunaar@gmail.com>
2025-11-21 11:48:41 +0545

    feat:upload and login

---
+++ backend/routes/admin.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { Router } from "express";
+import auth from "../middleware/auth.js";
+import multer from "multer";
+import path from "path";
+
+const router = express.Router();
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (username === "admin" && password === "pass123") {
+    res.json({ token: "1234", message: "Login sucessfully" });
+  } else {
+    res.status(401).json({ message: "Invalid credentials" });
+  }
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const folder = file.mimetype.startsWith("video") ? "videos" : "images";
+    cb(null, path.join("media", folder));
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+router.post("/upload", upload.single("file"), (req, res) => {
+  console.log("test", req.body);
+
+  if (!req.file) {
+    return res.status(400).json({ message: "no file uploaded!" });
+  }
+  res
+    .status(200)
+    .json({ message: `${req.file.filename} uploaded sucessfully.` });
+});
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,13 @@
 // backend/server.js
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-const multer = require("multer");
-const cors = require("cors");
+import cors from "cors";
+import express from "express";
+import adminRoutes from "./routes/admin.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Serve media
-app.use("/videos", express.static(path.join(__dirname, "media/videos")));
-app.use("/images", express.static(path.join(__dirname, "media/images")));
-
-// Notices API
-app.get("/api/notices", (req, res) => {
-  const notices = JSON.parse(fs.readFileSync("./notices.json"));
-  res.json(notices);
-});
-
-// File upload for admin
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const folder = file.fieldname === "video" ? "videos" : "images";
-    cb(null, path.join(__dirname, "media", folder));
-  },
-  filename: (req, file, cb) => cb(null, file.originalname),
-});
-const upload = multer({ storage });
-
-app.post("/admin/upload", upload.single("file"), (req, res) => {
-  res.json({ message: "File uploaded successfully" });
-});
-
-app.post("/admin/notices", (req, res) => {
-  fs.writeFileSync("./notices.json", JSON.stringify(req.body.notices, null, 2));
-  res.json({ message: "Notices updated successfully" });
-});
-
+app.use("/admin", adminRoutes);
 const PORT = 3000;
 app.listen(PORT, () =>
   console.log(`Backend running on http://localhost:${PORT}`)
